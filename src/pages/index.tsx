7036030389a0fc9dc110f90f05a88e3b0a5e7cc3
Daniel Costa <dancosta.id@gmail.com>
2022-05-10 15:06:24 -0300

    halfway through the graphQL use

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,61 @@
-import { Heading } from '@chakra-ui/react'
-import { Center } from '@chakra-ui/react'
+import { Heading, Center, Box, Text } from '@chakra-ui/react'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql
+} from "@apollo/client"
 
-export default function Home() {
+export default function Home({ company, rockets }) {
+  console.log('launches:', company, 'rocket:', rockets)
   return (
     <>
       <Center bg='purple.700' h='100px' color='white'>
-        <Heading as='h1'>Welcome to the Marvolous SpaceX App</Heading>
+        <Heading as='h1'>SpaceX App</Heading>
+      </Center>
+      <Center>
+        <Box borderRadius={8} bg='purple.50' w='80vw' h='80vh' mt={8} p={4}>
+          <Heading>Latest from launches SpaceX</Heading>
+
+          <Text mt={10}>This is a text</Text>
+        </Box>
       </Center>
     </>
   )
 }
 
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: 'https://api.spacex.land/graphql/',
+    cache: new InMemoryCache()
+  })
 
-// - Home:
-// - Opção para ver informações da empresa;
-// - Opção para buscar por foguetes;
+  const { data } = await client.query({
+    query: gql `
+      query SpaceData {
+        company {
+          ceo
+          coo
+          founded
+          valuation
+          employees
+        }
+        rockets {
+          company
+          name
+          description
+          cost_per_launch
+        }
+      }
+    `
+  })
+
+  console.log('data', data)
+
+  return {
+    props: {
+      company: data.company,
+      rockets: data.rockets,
+    }
+  }
+}
