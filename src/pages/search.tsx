5c33729bd8893b6bbfebb9f684020789987b16aa
Daniel Costa <dancosta.id@gmail.com>
2022-05-14 10:25:42 -0300

    I've tried one approach for the searchbar. But I'm saving, because I'm going to try a different one before deciding

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -37,7 +37,6 @@ useEffect(() => {
     //  Api to search results
     setValue(searchTerm)
     console.log('Search', searchTerm)
-    return <Box><Text>{searchTerm}</Text></Box>
   }
 
 
@@ -74,83 +73,6 @@ useEffect(() => {
               onClick={() => onSearch(value)}
             />
             </Flex>
-            <Box 
-              border={2}
-              borderColor='purple.100'
-              borderRadius={8}
-              w='60vw'  
-              p={4}
-            >
-              {rocket.filter((item) => {
-                  const searchTerm = value.toLowerCase()
-                  const name = item.name.toLowerCase()
-
-                  return searchTerm && name.startsWith(searchTerm) && name !== searchTerm
-              })
-                .slice(0,10)  
-                .map((item, key) => { 
-                return(
-                  <Grid templateColumns='repeat(1, 1fr)' gap={4}>
-                    <GridItem 
-                      key={key} 
-                      bg="white" 
-                      p={4} 
-                      m={4} 
-                      borderRadius={10} 
-                      boxShadow='xl' 
-                      cursor='pointer'
-                      onClick={() => onSearch(item.name)}
-                      _hover={{
-                        border: '1px solid'
-                      }}
-                    >
-                      <UnorderedList spacing={3} ms={6} mt={4} gap={4} styleType="none">
-                        <ListItem>
-                            <ListIcon as={FaSpaceShuttle} color='purple.900' fontSize={34}/>
-                            <Flex alignItems="center">
-                              <Text as='i' mr={4} color="purple.400">Nome do Foguete:</Text> 
-                              <Text color="gray.500">{item.name}</Text>
-                            </Flex>
-                          </ListItem>
-
-                          <ListItem>
-                            <Flex alignItems="center">
-                              <Flex flexDirection="column">
-                                <Text as='i' mr={4} color="purple.400">Descrição:</Text>
-                                <Text color="gray.500">
-                                  {item.description}
-                                </Text>
-                              </Flex>
-                            </Flex>
-                          </ListItem>
-
-                          <ListItem>
-                          <Flex alignItems="center">
-                            <Text as='i' mr={4} color="purple.400">Custo de Lançamento:</Text>  
-                            <CurrencyFormat 
-                              value={item.cost_per_launch} 
-                              color="white"
-                              displayType={'text'} 
-                              thousandSeparator={true} 
-                              prefix={'$'} 
-                            />
-                          </Flex>
-                        </ListItem>
-
-                          <ListItem>
-                            <Flex alignItems="center">
-                              <Text as='i' mr={4} color="purple.400">Empresa responsável:</Text> 
-                              <Text color="gray.500">{item.company}</Text>
-                            </Flex>
-                          </ListItem>
-                      </UnorderedList>
-                    </GridItem>
-                  </Grid>
-                
-                )
-                })}
-          
-            </Box>
           </Flex>
         </Center>
 
@@ -162,10 +84,26 @@ useEffect(() => {
         </Center>
         <Center>
           <Grid templateColumns='repeat(2, 1fr)' gap={4}> 
+          {rocket.filter((item) => {
+                  const searchTerm = value.toLowerCase()
+                  const name = item.name.toLowerCase()
 
-          {rocket.map((value, key) =>{            
+                  return searchTerm && name.startsWith(searchTerm) && name !== searchTerm
+            })
+            .map((value, key) =>{            
               return(
-                <GridItem key={key} bg="white" p={4} m={4} borderRadius={10} boxShadow='xl'>
+                <GridItem 
+                  key={key} 
+                  bg="white" 
+                  p={4} m={4} 
+                  borderRadius={10} 
+                  boxShadow='xl'
+                  cursor='pointer'
+                  onClick={() => onSearch(value.name)}
+                  _hover={{
+                    border: '1px solid'
+                  }}
+                >
                   <UnorderedList spacing={3} ms={6} mt={4} gap={4} styleType="none">
                     <ListItem>
                         <ListIcon as={FaSpaceShuttle} color='purple.900' fontSize={34}/>
@@ -212,18 +150,6 @@ useEffect(() => {
 
           </Grid>
         </Center>
-        {/* back/next buttons */}
-        <Flex mb={12} mx={8} pb={4}>
-        <Box>
-          <Link href='/about'>Voltar</Link>
-        </Box>
-
-        <Spacer />
-
-        <Box>
-          <Link href='/'>Home</Link>
-        </Box>
-      </Flex>
       <Footer />
     </>
   )
